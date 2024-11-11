6ac7542f7a3e685aeeccacf707c5ffc03aaf0d6f
Packmangame <157449270+Packmangame@users.noreply.github.com>
2024-11-11 13:33:45 +0500

    Add files via upload

---
+++ Olimp/Romanov_Andrey_PR_31_zd_5/Romanov_Andrey_PR_31_zd_5/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Romanov_Andrey_PR_31_zd_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int PetiaLenght, MessLength;
+            string[] line = File.ReadAllLines("input.txt");
+            PetiaLenght = int.Parse(line[0].Substring(0, line[0].LastIndexOf(' ')));
+            MessLength = int.Parse(line[0].Substring(line[0].LastIndexOf(' ')));
+
+
+            StreamWriter sw = new StreamWriter("output.txt");
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine();
+            Console.ResetColor();
+            sw.WriteLine();
+            sw.Close();
+            Console.WriteLine("Press enter");
+            Console.Read();
+            Environment.Exit(0);
+        }
+
+    }
+}
